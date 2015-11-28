c5763373c8d8ac4925b5626d64f9a5cd4244431a
nathanhamilton <esquireofthelord@gmail.com>
2015-11-28 00:12:07 -0600

    Extract randomize class

--- dominion-randomizer.rb
+++ dominion-randomizer.rb
@@ -1,47 +1,10 @@
 dominion_path = File.expand_path('../', __FILE__)
 $LOAD_PATH.unshift(dominion_path) unless $LOAD_PATH.include?(dominion_path)
 
-class Dominion
-
-  require 'dominion-basic-cards'
-      
-  attr_reader :number_of_cards
-  attr_accessor :selected_cards
-
-  def initialize(number_of_cards = 10)
-    @number_of_cards = number_of_cards
-    @selected_cards = []
-    randomize_set
-  end
-
-  def randomize_set
-    randomize 
-    selected_cards.map { |card| puts "#{card[:name]} : #{card[:type]}" }
-  end
-
-  private
-
-  def randomize
-    i = 0
-    while i < number_of_cards
-      rand_obj = Random.new
-      random_number = rand_obj.rand(basic_cards.keys.last)
-      unless selected_cards.include?(basic_cards[random_number])
-        if basic_cards[random_number] != nil
-          selected_cards << basic_cards[random_number]
-          i += 1
-        end
-      end
-    end
-  end
-
-  def basic_cards
-    DominionBasic::BASIC_CARDS
-  end
-end
+require 'randomize'
 
 puts "Starting up dominion randomizor"
 puts "Here is your deck:"
 
-Dominion.new
+Randomize.new
 
---
+++ randomize.rb
@@ -0,0 +1,38 @@
+class Randomize
+
+  require 'dominion-basic-cards'
+
+  attr_reader :number_of_cards
+  attr_accessor :selected_cards
+
+  def initialize(number_of_cards = 10)
+    @number_of_cards = number_of_cards
+    @selected_cards = []
+    randomize_set
+  end
+
+  def randomize_set
+    select_cards
+    selected_cards.map { |card| puts "#{card[:name]} : #{card[:type]}" }
+  end
+
+  private
+
+  def select_cards
+    i = 0
+    while i < number_of_cards
+      rand_obj = Random.new
+      random_number = rand_obj.rand(basic_cards.keys.last)
+      unless selected_cards.include?(basic_cards[random_number])
+        if basic_cards[random_number] != nil
+          selected_cards << basic_cards[random_number]
+          i += 1
+        end
+      end
+    end
+  end
+
+  def basic_cards
+    DominionBasic::BASIC_CARDS
+  end
+end
